a7d9246548c8e71dc8f8447f56f91f94ec1a5395
Hamzah <dehamzah@gmail.com>
2021-04-18 11:06:32 +0700

    chore: init project

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import bgTop from "../images/bg1.jpg";
+import "./index.css";
+
+const IndexPage = () => {
+  return (
+    <div className="container-page">
+      <img className="bg-top" src={bgTop} />
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
