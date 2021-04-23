1a7428b5884d66e25b2c2fb522da41e7f3597595
Hamzah <dehamzah@gmail.com>
2021-04-23 23:24:07 +0700

    feat: add google calendar url

--- src/components/DateCountSection.js
+++ src/components/DateCountSection.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { googleCalendarEventUrl } from "google-calendar-url";
 
 import "./DateCountSection.css";
 
 const DateCountSection = () => {
+  const url = googleCalendarEventUrl({
+    start: "20210530T030000Z",
+    end: "20210530T040000Z",
+    title: "Akad Nikah Fafa & Hamzah",
+    location: "Kaliangkrik, Magelang, Central Java, Indonesia",
+  });
+
   return (
     <div className="text-center pb-10">
       <div className="time-item-container pt-8 color-text-grey font-heading">
@@ -24,7 +32,8 @@ const DateCountSection = () => {
         </div>
       </div>
       <a
-        href="#"
+        target="_blank"
+        href={url}
         class="mt-6 font-body font-thin text-sm inline-flex items-center justify-center px-5 py-1 border border-tosca rounded-full color-text-purple bg-white "
       >
         INGATKAN SAYA
