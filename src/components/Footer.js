f8d5342eaed4ab6b60c12864289bdf9c96fa1831
Fafa Hamzah <fafahamzah30mei@gmail.com>
2021-04-24 17:02:42 +0700

    feat: add background sound

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,6 +25,24 @@ const Footer = () => {
           @charupin
         </a>
       </p>
+      <div className="text-center pt-6 audio-container">
+        <audio controls autoplay loop>
+          <source
+            src="radwimps_-_is_there_still_anything_that_love_can_do.mp3"
+            type="audio/mpeg"
+          />
+          Your browser does not support the audio element.
+        </audio>
+        <p className="text-xs font-body color-text-grey pt-2">
+          <a
+            href="https://www.youtube.com/watch?v=-u_T8Z2S2ng"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Is There Anything That Love Can Do - Radwimps
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
